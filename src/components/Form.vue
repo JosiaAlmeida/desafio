1b908ea39595d139414eb5f4d2fdf98ae3885d72
unclereggie <josiaalmeida0001@gmail.com>
2021-08-05 22:28:18 +0100

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <form >
+            <input type="text" placeholder="Digite aqui...">
+            <div v-for="dados in data" :key="dados.id">
+                <Slug :data="" />
+                <input type="checkbox" name="inp" id="">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script lang='ts'>
+import axios from 'axios'
+import Slug from './slug/slug.vue'
+
+import {Component, Prop, Vue} from 'vue-property-decorator'
+export default Vue.extend({
+    name: 'Form'
+    components:{
+        Slug
+    }
+})
+</script>
---
+++ src/components/slug/slug.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <form >
+            <input type="text" placeholder="Digite aqui...">
+            <div v-for="dados in data" :key="dados.id">
+                <label for="">Nome</label>
+                <input type="checkbox" name="inp" id="">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script lang='ts'>
+
+import { Prop, Vue} from 'vue-property-decorator'
+export default Vue.extend({
+    name: 'Slug',
+    props:{
+        data: {
+            type: Object
+        }
+    }
+})
+</script>
