82265cf458c059f0b6d42105e0243af6dc318ba6
Josia Almeida <josiaalmeida0001@gmail.com>
2021-08-07 20:35:09 +0100

    first commit

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,7 +3,7 @@
         <form >
             <input type="text" placeholder="Digite aqui...">
             <div v-for="dados in data" :key="dados.id">
-                <Slug :data="" />
+                <Slug :data="data" />
                 <input type="checkbox" name="inp" id="">
             </div>
         </form>
@@ -15,10 +15,32 @@ import axios from 'axios'
 import Slug from './slug/slug.vue'
 
 import {Component, Prop, Vue} from 'vue-property-decorator'
+interface filterOptions {
+    subline: string,
+    value: string,
+    label: string,
+    type: string,
+    queryField: string,
+    tags: string
+}
 export default Vue.extend({
-    name: 'Form'
+    name: 'Form',
     components:{
         Slug
+    },
+    data(){
+        return{
+            data: [] as filterOptions[]
+        }
+    },
+    methods:{
+        async AllData(){
+            await axios.get<any>('https://filters.app3.speedio.com.br/api/v3/filters.json')
+            .then(res=> res.data.filters.map(x=> console.log(x.filters)))
+        }
+    },
+    mounted(){
+        this.AllData()
     }
 })
 </script>
