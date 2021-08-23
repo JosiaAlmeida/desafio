1e982da4d174e5913bdb0fc701f93f5580834924
Josia Almeida <josiaalmeida0001@gmail.com>
2021-08-23 16:46:36 +0100

    Update roles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,46 +1,92 @@
 <template>
-    <div>
-        <form >
-            <input type="text" placeholder="Digite aqui...">
-            <div v-for="dados in data" :key="dados.id">
-                <Slug :data="data" />
-                <input type="checkbox" name="inp" id="">
-            </div>
-        </form>
-    </div>
+  <div>
+    <form>
+      <input type="text" placeholder="Digite aqui..." v-model="keyword" />
+      <Slug :keyword="keyword" :data="Data" :isShow="isShow" />
+    </form>
+  </div>
 </template>
 
 <script lang='ts'>
-import axios from 'axios'
-import Slug from './slug/slug.vue'
+import axios from "axios";
+import Slug from "./slug/slug.vue";
 
-import {Component, Prop, Vue} from 'vue-property-decorator'
+import { Vue } from "vue-property-decorator";
 interface filterOptions {
-    subline: string,
-    value: string,
-    label: string,
-    type: string,
-    queryField: string,
-    tags: string
+  subline: string;
+  value: string;
+  label: string;
+  type: string;
+  queryField: string;
+  tags: string;
+}
+interface Filters {
+  groupTag: string
+  
 }
 export default Vue.extend({
-    name: 'Form',
-    components:{
-        Slug
+  name: "Form",
+  components: {
+    Slug,
+  },
+  data() {
+    return {
+      Data: [] as filterOptions[],
+      keyword: '' as string,
+      isShow: false,
+      Result: [] as filterOptions[]
+    };
+  },
+  methods: {
+    async AllData() {
+      await axios
+        .get("https://filters.app3.speedio.com.br/api/v3/filters.json")
+        .then((res) => {
+          let filters = res.data.filters.map((x: any) =>
+            x.filters
+              .filter(
+                (x: Filters) =>
+                  x.groupTag == "setores e cnaes" ||
+                  x.groupTag == "estados" ||
+                  x.groupTag == "municipio"
+              )
+              .pop()
+          );
+          let filterOptions = filters;
+          this.ReducerElementArray(filterOptions);
+          let data: filterOptions[] = [];
+          filterOptions.map((x: any) => data.push(x.filterOptions));
+          data.map((x: any) =>
+            x.slice(0, 7).map((x: any) => this.Data.push({ ...x, bool: false }))
+          );
+        })
+        .catch((err) => console.log("Ocorreu um erro " + err));
     },
-    data(){
-        return{
-            data: [] as filterOptions[]
+    ReducerElementArray(value: filterOptions[]){
+      var i = 0;
+      while (i < value.length) {
+        if (value[i] === undefined) {
+          value.splice(i, 1);
+        } 
+        else {
+          ++i;
         }
+      }
     },
-    methods:{
-        async AllData(){
-            await axios.get<any>('https://filters.app3.speedio.com.br/api/v3/filters.json')
-            .then(res=> res.data.filters.map(x=> console.log(x.filters)))
-        }
+    VerifyKeywordData(){
+      if(this.keyword)
+        this.isShow = true
+      else this.isShow = false
     },
-    mounted(){
-        this.AllData()
-    }
-})
+    TransformText(text: string){
+      return text.normalize("NFD").replace(/[^a-zA-Zs]/g, "").toLowerCase()
+    },
+  },
+  mounted() {
+    this.AllData();
+  },
+  updated(){
+    this.VerifyKeywordData()
+  }
+});
 </script>
--- src/components/slug/slug.vue
+++ src/components/slug/slug.vue
@@ -1,24 +1,105 @@
 <template>
-    <div>
-        <form >
-            <input type="text" placeholder="Digite aqui...">
-            <div v-for="dados in data" :key="dados.id">
-                <label for="">Nome</label>
-                <input type="checkbox" name="inp" id="">
-            </div>
-        </form>
-    </div>
+  <div>
+    <form>
+      <div v-if="isShow">
+        <div v-for="(dados, i) in Result" :key="i">
+          <label for=""> {{ dados.label }} </label>
+          <input
+            type="checkbox"
+            :value="dados.value"
+            v-model="dados.bool"
+            @change="Save()"
+          />
+        </div>
+      </div>
+      <div v-else>
+        <div v-for="(dados, i) in AllDatas" :key="i">
+          <label for=""> {{ dados.label }} </label>
+          <input
+            type="checkbox"
+            :value="dados.value"
+            v-model="dados.bool"
+            @change="Save()"
+          />
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang='ts'>
+const STOREGE_KEY = "STOREGE_KEY";
+import { Vue } from "vue-property-decorator";
+interface Data {
+  id: number;
+  label: string;
+  value: string;
+  subline: string;
+  isSelected?: boolean;
+}
 
-import { Prop, Vue} from 'vue-property-decorator'
 export default Vue.extend({
-    name: 'Slug',
-    props:{
-        data: {
-            type: Object
-        }
+  name: "Slug",
+  props: {
+    data: {
+      type: Array,
+    },
+    ["keyword"]: String,
+    ["isShow"]: Boolean
+  },
+  data() {
+    return {
+      AllDatas: [] as Data[],
+      Result: [] as Data[]
+    };
+  },
+  methods: {
+    Save() {
+      localStorage.setItem(STOREGE_KEY, JSON.stringify(this.AllDatas));
+    },
+    NewData() {
+      let _data: any = this.data;
+      this.AllDatas = _data;
+    },
+    VerifySelected() {
+      this.AllDatas = this.AllDatas.filter(
+        (x: Data) => !this.Result.includes({ ...x })
+      ).concat(
+        this.Result.filter((x: Data) => !this.AllDatas.includes({ ...x }))
+      );
+      this.Save();
+    },
+    searchData() {
+      this.Result = this.keyword
+        ? this.AllDatas.filter(
+            (x) =>
+              this.TransformText(x.label).includes(
+                this.TransformText(this.keyword)
+              ) ||
+              x.subline.includes(this.keyword)
+          )
+        : this.Result;
+    },
+    TransformText(text: string) {
+      return text
+        .normalize("NFD")
+        .replace(/[^a-zA-Zs]/g, "")
+        .toLowerCase();
+    },
+  },
+
+  mounted() {
+    const data: any = localStorage.getItem(STOREGE_KEY);
+    if (localStorage.getItem(STOREGE_KEY)) {
+      this.AllDatas = JSON.parse(data);
     }
-})
+  },
+  created() {
+    if(!this.keyword) this.NewData();
+    if (this.isShow) this.VerifySelected();
+  },
+  beforeUpdate() {
+    this.searchData();
+  },
+});
 </script>
